a50f4a0de12d8ba84706c5db3d449a6ff7fdc20c
bhumur-rough <agrawalbhumur@gmail.com>
2025-06-07 13:16:39 +0530

    fix

---
+++ NewServlet/src/main/java/com/cdac/acts/FirstServlet.java
@@ -0,0 +1,27 @@
+package com.cdac.acts;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("Data : " + new Date());
+		}
+}
---
+++ serveletOldMethod/src/serveletOldMethod/Myfirst.java
@@ -0,0 +1,22 @@
+package serveletOldMethod;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.rmi.ServerException;
+import java.util.Date;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Myfirst extends HttpServlet{
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse respond) throws IOException, ServerException {
+		PrintWriter out = respond.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1> Current time is : " + new Date() + "</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
