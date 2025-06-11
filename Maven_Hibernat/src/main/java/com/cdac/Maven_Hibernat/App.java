fa034cbb498db8debbd4bdb02ee8b25c015c67db
bhumur-rough <agrawalbhumur@gmail.com>
2025-06-11 21:48:13 +0530

    hibernate

--- Maven_Hibernat/src/main/java/com/cdac/Maven_Hibernat/App.java
+++ Maven_Hibernat/src/main/java/com/cdac/Maven_Hibernat/App.java
@@ -1,4 +1,4 @@
-package com.cdac.FirstMaven;
+package com.cdac.Maven_Hibernat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,8 +8,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import com.cdac.entity.User;
-
-import jakarta.persistence.FindOption;
 
 /**
  * Hello world!
@@ -23,23 +21,35 @@ public class App
         hibernateConfig.configure("first.cfg.xml");
         SessionFactory hibernateFactory = hibernateConfig.buildSessionFactory();
         Session hibernateSession = hibernateFactory.openSession();
-        
-//        User obj = new User("shubhu","shubh@123","shubham","shubham@gmail","kolhapur");
+        //persist
+        User obj = new User("hellobhumur","hellobhumur","Bhumur Agrawal","bhumur@gmail.com","Jaipur");
 //        hibernateSession.beginTransaction();
 //        hibernateSession.persist(obj);
 //        hibernateSession.getTransaction().commit();
         
-        
-//        User user = hibernateSession.find(User.class, "ADMiN");
+        //find
+//        User user = hibernateSession.find(User.class, "admin");
 //        System.out.println(user);
         
+        //merge
+//        hibernateSession.beginTransaction();
+//        hibernateSession.merge(obj);
+//        hibernateSession.getTransaction().commit();
+        
+        //remove
+//        hibernateSession.beginTransaction();
+//        hibernateSession.remove(obj);
+//        hibernateSession.getTransaction().commit();
         
         
+        //load
+        hibernateSession.load(obj,"bhumur");
+	    System.out.println(obj);
         
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("admin");
-        list.add("bhumur");
-        list.add("shubhu");
+//        ArrayList<String> list = new ArrayList<String>();
+//        list.add("admin");
+//        list.add("bhumur");
+//        list.add("shubhu");
 //        FindOption options = new FindOption(true, "name", 10, 0);
 //        List<User> multiple = hibernateSession.findMultiple(User.class, list, FindOption.class);
 //        System.out.println(multiple);
@@ -49,7 +59,6 @@ public class App
 //                .setParameter("name", list)
 //                .getResultList();
 //        System.out.println(users);
-        
         
     }
 }
--- Maven_Hibernat/src/test/java/com/cdac/Maven_Hibernat/AppTest.java
+++ Maven_Hibernat/src/test/java/com/cdac/Maven_Hibernat/AppTest.java
@@ -1,4 +1,4 @@
-package com.cdac.FirstMaven;
+package com.cdac.Maven_Hibernat;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
