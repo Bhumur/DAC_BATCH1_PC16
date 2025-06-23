a97114dc11c8489c447d27b1b019c390defcafc7
bhumur-rough <agrawalbhumur@gmail.com>
2025-06-23 19:17:25 +0530

    maybelastpush

--- eSubmit_Backend_final/src/main/java/com/employee/entity/Employee.java
+++ eSubmit_Backend_final/src/main/java/com/employee/entity/Employee.java
@@ -3,13 +3,17 @@ package com.employee.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "employees") 
 @Data // Lombok: Generates getters, setters, toString, equals, hashCode
 @NoArgsConstructor // Lombok: Generates a no-argument constructor
 @AllArgsConstructor // Lombok: Generates a constructor with all arguments
+@Getter
+@Setter
 public class Employee {
 
     @Id 
